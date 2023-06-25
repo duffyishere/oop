9c9e2c86b09a9c94c646de5680e73acf05ad9c83
zzunho9 <jamlove4763@naver.com>
2023-06-25 19:48:28 +0900

    feat: Add a number to card

--- src/main/java/org/duffy/cardGame/Card.java
+++ src/main/java/org/duffy/cardGame/Card.java
@@ -1,3 +1,7 @@
 package org.duffy.cardGame;
 
-public abstract class Card implements Comparable {}
+public abstract class Card implements Comparable {
+    private int number;
+
+    public int getNumber() {return this.number;}
+}
--- src/main/java/org/duffy/cardGame/Player.java
+++ src/main/java/org/duffy/cardGame/Player.java
@@ -4,4 +4,6 @@ public abstract class Player {
     private String name;
 
     public String getName() {return this.name;}
+
+    public abstract void play();
 }
