7cfdbc20297c44c9e265ec634b12ff401c17c68b
zzunho9 <jamlove4763@naver.com>
2023-07-02 01:23:03 +0900

    feat: Add Suite class

---
+++ src/main/java/org/duffy/cardGame/PlayingCard.java
@@ -0,0 +1,12 @@
+package org.duffy.cardGame;
+
+import org.duffy.cardGame.Card;
+
+public class TrumpCard extends Card {
+
+    private
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/duffy/cardGame/Suite.java
@@ -0,0 +1,10 @@
+package org.duffy.cardGame;
+
+public enum Suite {
+    SPADE,
+    HEART,
+    DIAMOND,
+    CLUB,
+    ACE,
+    JOKER
+}
